d63568678ef60600819283d2bc9fc68f29db327e
Emmanuel Guefif <eguefif@fastmail.com>
2026-06-05 09:18:48 +0200

    pento/elixir: add redirect to guess

--- elixir/liveview_book/pento/lib/pento_web/controllers/page_controller.ex
+++ elixir/liveview_book/pento/lib/pento_web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule PentoWeb.PageController do
   use PentoWeb, :controller
 
-  def home(conn, _params) do
+  def home(conn, _params) when is_nil(conn.current_scope) do
     render(conn, :home)
+  end
+
+  def home(conn, _params) do
+    redirect(conn, to: "/guess")
   end
 end
--- elixir/liveview_book/pento/lib/pento_web/router.ex
+++ elixir/liveview_book/pento/lib/pento_web/router.ex
@@ -56,6 +56,7 @@ defmodule PentoWeb.Router do
       live "/users/settings", UserLive.Settings, :edit
       live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
       live "/guess", WrongLive
+      live "/", WrongLive
     end
 
     post "/users/update-password", UserSessionController, :update_password
