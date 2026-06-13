9985848496f4c30440e7099e7037c54cdb1f2457
Emmanuel Guefif <eguefif@fastmail.com>
2026-06-13 09:46:31 +0200

    Greed/ruby: refactor to handle kinds in one pass

--- katas/greed/ruby/lib/greed.rb
+++ katas/greed/ruby/lib/greed.rb
@@ -1,34 +1,61 @@
 class Greed
   attr_reader
 
+  TRIPLE_DICE = {
+    1 => 1000, 
+    2 => 200, 
+    3 => 300, 
+    4 => 400, 
+    5 => 500,
+    6 => 600
+  }
+
+  MULTIPLIERS_KIND = {
+    0 => 0,
+    1 => 0,
+    2 => 0,
+    3 => 1,
+    4 => 2,
+    5 => 4,
+    6 => 8,
+  }
+
   def initialize; end
 
   def self.is_triples(tallies)
     return false
   end
 
-  def self.take_six_of_a_kind(score, dices)
+  def self.take_straight(score, dices)
     return score, dices if dices.length == 0
 
-    tallies = dices.tally
-    sixes = tallies.select { |k, v| v == 6 }
-    dice = sixes.keys.first
-
-    return score, dices if dice.nil?
-    return dice * 100 * 8, [] 
-  end
-
-  def self.take_straight(score, dices)
     [1, 2, 3, 4, 5, 6].each do |n|
       return score, dices unless dices.any?(n)
     end
     return score + 1200, []
   end
 
+  def self.get_kind_score(dice, kind)
+    base_score = TRIPLE_DICE[dice]
+    multiplier = MULTIPLIERS_KIND[kind]
+
+    base_score * multiplier
+  end
+
+  def self.take_x_of_a_kind(score, dices)
+    tallies = dices.tally
+    tallies.each do |dice, kind|
+      score += get_kind_score(dice, kind)
+      dices = dices.reject { |d| d == dice } if kind >= 3
+    end
+    return score, dices
+  end
+
   def self.score(dices)
     score = 0
+    tallies = dices.tally
     score, dices = take_straight(score, dices) 
-    score, dices = take_six_of_a_kind(score, dices) 
+    score, dices = take_x_of_a_kind(score, dices) 
 
     return score
   end
--- katas/greed/ruby/test/greed_test.rb
+++ katas/greed/ruby/test/greed_test.rb
@@ -13,7 +13,20 @@ class GreedTest < Minitest::Test
   def test_straight
     assert_equal 1200, Greed.score([1, 2, 3, 4, 5, 6])
   end
+
   def test_six_of_a_kind_5
     assert_equal 4000, Greed.score([5, 5, 5, 5, 5, 5])
+  end
+
+  def test_five_of_a_kind_5_no_signle_one_no_fifty
+    assert_equal 1600, Greed.score([4, 4, 4, 4, 4, 2])
+  end
+
+  def test_four_of_a_kind_4_no_signle_one_no_fifty
+    assert_equal 800, Greed.score([4, 4, 4, 4, 3, 2])
+  end
+
+  def test_triple_2_no_signle_one_no_fifty
+    assert_equal 200, Greed.score([2, 2, 2, 4, 3, 4])
   end
 end
