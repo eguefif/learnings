251166521ba74cec3ce751e7f45e7a9be8e02a01
Emmanuel Guefif <eguefif@fastmail.com>
2026-06-17 21:34:08 +0200

    poker/elixir: add hands comparaison

--- katas/poker_hands/elixir/lib/card.ex
+++ katas/poker_hands/elixir/lib/card.ex
@@ -18,6 +18,27 @@ defmodule PokerHands.Card do
   }
   @suit %{"D" => :diamond, "S" => :spades, "H" => :heart, "C" => :club}
 
+  @values %{
+    :two => 2,
+    :three => 3,
+    :four => 4,
+    :five => 5,
+    :six => 6,
+    :seven => 7,
+    :eight => 8,
+    :nine => 9,
+    :ten => 10,
+    :jack => 11,
+    :queen => 12,
+    :king => 13,
+    :as => 14
+  }
+
+  @doc """
+    Create a new Card struct from a string.
+
+    Returns %PokerHands.Card{}
+  """
   def new(card) when is_bitstring(card) do
     %PokerHands.Card{ranking: card_to_ranking(card), suit: card_to_suit(card)}
   end
@@ -30,5 +51,25 @@ defmodule PokerHands.Card do
   defp card_to_suit(card) do
     suit_str = card |> String.graphemes() |> Enum.at(1)
     Map.get(@suit, suit_str)
+  end
+
+  def compare(card1, card2) when is_bitstring(card1) and is_bitstring(card2) do
+    card1 = PokerHands.Card.new(card1)
+    card2 = PokerHands.Card.new(card2)
+    PokerHands.Card.compare(card1, card2)
+  end
+
+  def compare(
+        %PokerHands.Card{ranking: r1, suit: _} = _,
+        %PokerHands.Card{ranking: r2, suit: _} = _
+      ) do
+    v1 = Map.fetch!(@values, r1)
+    v2 = Map.fetch!(@values, r2)
+
+    cond do
+      v1 == v2 -> :eq
+      v1 > v2 -> :card1
+      v1 < v2 -> :card2
+    end
   end
 end
--- katas/poker_hands/elixir/test/card_test.exs
+++ katas/poker_hands/elixir/test/card_test.exs
@@ -1,10 +1,21 @@
 defmodule CardTest do
-  use ExUnit.Case
   alias PokerHands.Card
+
+  use ExUnit.Case,
+    async: false,
+    parameterize:
+      for(
+        {card1, card2, expect} <- [{"2C", "3H", :card2}, {"AD", "KS", :card1}, {"TH", "TC", :eq}],
+        do: %{card1: card1, card2: card2, expect: expect}
+      )
 
   test "should returns card struct from string" do
     %Card{ranking: ranking, suit: suit} = Card.new("AC")
     assert ranking == :as
     assert suit == :club
+  end
+
+  test "should compare cards", %{card1: c1, card2: c2, expect: expected} do
+    assert Card.compare(c1, c2) == expected
   end
 end
